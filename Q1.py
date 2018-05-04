09afcef801ed3ae918576336485b5b189779554e
yonio <yoni.osin@gmail.com>
2018-05-04 12:12:26 +0300

    added getGaussPyamid

--- Q1.py
+++ Q1.py
@@ -13,16 +13,16 @@ def getGaussPyramid(image, levels):
 def getLaplasPyramid(image, levels):
     g = getGaussPyramid(image, levels)
 
-    L = {}
+    l = {}
     for i in range(1, levels + 1):
         if i == 1:
-            L[i] = image.astype(int) - g[2]
+            l[i] = image.astype(int) - g[2]
         elif i == levels:
-            L[i] = g[levels]
+            l[i] = g[levels]
         else:
-            L[i] = g[i] - g[i + 1]
+            l[i] = g[i] - g[i + 1]
 
-    return L
+    return l
 
 
 def reconstructPyramid(pyramid_levels):
