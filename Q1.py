9117ae46e5504ca535a6e527ffb9814a43e436d6
yonio <yoni.osin@gmail.com>
2018-05-04 12:10:36 +0300

    added getGaussPyamid

--- Q1.py
+++ Q1.py
@@ -3,34 +3,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def getPyramid(image, levels):
-    L = [np.empty(image.shape)] * levels
+def getGaussPyramid(image, levels):
+    g = {}
+    for i in range(2, levels + 1):
+        g[i] = cv.GaussianBlur(image, (0, 0), 2 ** i).astype(int)
+    return g
+
+
+def getLaplasPyramid(image, levels):
+    g = getGaussPyramid(image, levels)
+
+    L = {}
     for i in range(1, levels + 1):
         if i == 1:
-            L[i - 1] = image.astype(int) - cv.GaussianBlur(image, (0, 0), 2 ** 2)
-        elif i == n:
-            L[i - 1] = cv.GaussianBlur(image, (0, 0), 2 ** n).astype(int)
+            L[i] = image.astype(int) - g[2]
+        elif i == levels:
+            L[i] = g[levels]
         else:
-            L[i - 1] = cv.GaussianBlur(image, (0, 0), 2 ** i).astype(int) - cv.GaussianBlur(image, (0, 0), 2 ** i + 1)
+            L[i] = g[i] - g[i + 1]
 
     return L
 
 
 def reconstructPyramid(pyramid_levels):
-    img_size = pyramid_levels[0].shape
-    result = np.zeros(img_size)
-    for i in range(len(pyramid_levels)):
+    img_size = pyramid_levels[1].shape
+    result = np.zeros(img_size, dtype=int)
+    for i in range(1, len(pyramid_levels) + 1):
         result += pyramid_levels[i]
 
-    return result.astype(np.int)
+    return result.astype(int)
 
 
 if __name__ == '__main__':
     img = cv.imread('noy.jpg', 0)
     n = 6
-    L = getPyramid(img, n)
-    normal = np.zeros(L[0].shape)
+    L = getLaplasPyramid(img, n)
 
-    plt.subplot(1,2,1), plt.imshow(reconstructPyramid(L), cmap='gray')
-    plt.subplot(1,2,2), plt.imshow(img, cmap='gray')
+    plt.subplot(1, 2, 1), plt.imshow(reconstructPyramid(L), cmap='gray'), plt.title('Reconstructed')
+    plt.subplot(1, 2, 2), plt.imshow(img, cmap='gray'), plt.title('Original')
     plt.show()
