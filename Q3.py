98fd2d39700085bae821662ce881fc7803bd23f4
NaamaPearl <naama.pearl@campus.technion.ac.il>
2018-06-13 21:57:23 +0300

    l

--- Q3.py
+++ Q3.py
@@ -3,8 +3,7 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
-import nms
-
+from numpy.random import permutation
 
 def extractImages(video_path):
     vidcap = cv.VideoCapture(video_path)
@@ -80,14 +79,12 @@ def cornerDetector(img):
     # Now draw them
     res = np.hstack((centroids,corners))
     res = np.int0(res)
-    tmp = res.shape[0]
-    for i in range(tmp):
-        img_to_plt = img
-        img_to_plt[res[i,1],res[i,0]]=[0,0,255]
-        img_to_plt[res[i,3],res[i,2]] = [255,0, 0]
-        plt.figure()
-        plt.imshow(img_to_plt)
 
+    # img[res[:,1],res[:,0]]=[0,0,255]
+    img[res[:,3],res[:,2]] = [0,255, 0]
+    plt.figure()
+    plt.imshow(img)
+    # plt.show()
 
     return np.int0(corners)
 
@@ -112,18 +109,90 @@ def mathc(img1,img2):
 
     plt.imshow(img3),plt.show()
 
+def calcAffineMatrix(reference_points, shifted_points):
+    reference_mat = np.zeros((6, 6))
+    res_vec = np.zeros(6)
+
+    for i in range(3):
+        reference_mat[2 * i, 0] = reference_points[i, 0] #x_r
+        reference_mat[2 * i, 1] = reference_points[i, 1] #y_r
+        reference_mat[2 * i, 4] = 1 #b1
+
+        reference_mat[2 * i + 1, 2] = reference_points[i, 0]  # x_r
+        reference_mat[2 * i + 1, 3] = reference_points[i, 1]  # y_r
+        reference_mat[2 * i + 1, 5] = 1  # b2
+
+        res_vec[2 * i] = shifted_points[i, 0] #x
+        res_vec[2 * i + 1] = shifted_points[i, 1] #y
+
+    affine_parameters_vec = np.linalg.solve(reference_mat, res_vec)
+
+    return affine_parameters_vec
+
+
+def initChosenFeatures(corners):
+    chosen_features = np.empty((6, 3, 2), dtype=np.float32)
+
+    features_mat = [[1, 6, 13],[1, 9, 18],[1, 8, 14],[3, 10, 18],[1, 6, 13],[6, 13, 19]]
+
+    for i in range(6):
+        chosen_features[i, :, :] = corners[i][features_mat[i], :]
+
+    return chosen_features
+
+
+def calcAffineTrans(reference_pts, shifted_pts):
+    M = cv.getAffineTransform(reference_pts, shifted_pts)
+    iM = np.zeros(M.shape)
+    cv.invertAffineTransform(M, iM)
+    return M, iM
+
+
+def reconstractImgs(corners_features, list_of_frames):
+    reference_pts = chosen_features[0, :, :]
+
+    rows, cols, ch = list_of_frames[0].shape
+    M_list = []
+    iM_list = []
+    reconst_img = []
+
+    for i in range(1, len(list_of_frames)):
+        shifted_pts = chosen_features[i, :, :]
+        M, iM = calcAffineTrans(reference_pts, shifted_pts)
+        M_list.append(M)
+        iM_list.append(iM)
+        reconst_img.append(cv.warpAffine(frames[i], iM, (cols, rows)))
+
+    return reconst_img
+
+
+def plotRconstImg(input, output):
+    plt.subplot(121), plt.imshow(input), plt.title('Input')
+    plt.subplot(122), plt.imshow(output), plt.title('Output')
+    plt.show()
+
+
+def RANSAC(source_list_points, dst_list_poitns):
+    rand_pts_idx = permutation(len(source_list_points))[:3]
+    M, iM = calcAffineTrans(source_list_points[rand_pts_idx], source_list_points[rand_pts_idx])
+
 
 if __name__ == '__main__':
     # video_path = 'pasta.mp4'
     # extractImages(video_path)
     # makeVideoMask('extractedImgs')
     # createVideo('extractedImgs', 'masks', 'videoSeg.avi', 30)
-    frames_num = list(range(0, 10, 6))
+    frames_num = list(range(20, 100, 15))
     frames_names = ['extractedImgs/frame' + "%03d" % num + '.jpg' for num in frames_num]
     frames = [cv.imread(im) for im in frames_names]
     corners = [cornerDetector(img) for img in frames]
 
-    plt.show()
+    chosen_features = initChosenFeatures(corners)
+    reconst_img_list = reconstractImgs(chosen_features, frames)
+
+    [plotRconstImg(frame, reconst) for frame, reconst in zip(frames, reconst_img_list)]
+
+
 
     # [mathc(frames[0], frame) for frame in frames]
     print('all done')
