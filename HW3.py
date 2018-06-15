d130f3633f6eb8dca8830c2f985d10358c884f3e
NaamaPearl <naama.pearl@campus.technion.ac.il>
2018-06-15 21:15:47 +0300

    l

--- HW3.py
+++ HW3.py
@@ -3,23 +3,38 @@ from numpy.random import permutation
 import cv2 as cv
 import numpy as np
 from collections import namedtuple
+from Q3 import *
 
 
 class Frame:
     Point = namedtuple('Point', ['x', 'y'])
 
-    def __init__(self, idx, img):
+    def __init__(self, idx, img, reference_img):
         self.idx = idx
         self.img = img
         self.feature_points_vec = []
+        self.reference_img = reference_img
         self.cornerDetector()
+
+        self.img_with_harris = self.img
+        # for point in self.feature_points_vec:
+        #     self.ZeroPixelsInWindow(point, 10, self.img_with_harris, 255)
+
+    def ZeroPixelsInWindow(self, center, window_size, img, color):
+        try:
+            x_idx_vec, y_idx_vec = self.GetIndexes(center, window_size)
+        except ValueError:
+            return
+        for x_idx in x_idx_vec:
+            for y_idx in y_idx_vec:
+                img[y_idx ,x_idx] = [0, 0, color]
 
     @staticmethod
     def ThrowError():
         raise FrameError
 
     def GetIndexes(self, point, window_size):
-        max_possible_size = min(point.x, self.img.shape[0] - point.x, point.y, self.img.shape[1] - point.y)
+        max_possible_size = min(point.x, self.img.shape[1] - point.x, point.y, self.img.shape[0] - point.y)
         if window_size > max_possible_size:
             self.ThrowError()
 
@@ -29,7 +44,7 @@ class Frame:
         return x_idx, y_idx
 
     def GetWindow(self, point, window_size):
-        rows, cols = self.GetIndexes(point, window_size)
+        cols, rows = self.GetIndexes(point, window_size)
         res = self.img[rows]
         return res[:, cols, :]
 
@@ -40,7 +55,7 @@ class Frame:
 
     @staticmethod
     def ApplyAffineTrans(source_point, M):
-        return np.dot(M[:, :2], source_point) + M[:, -1]
+        return np.dot(M[:, :2], np.asarray([source_point.x, source_point.y])) + M[:, -1]
 
     def cornerDetector(self):
         gray = cv.cvtColor(self.img, cv.COLOR_BGR2GRAY)
@@ -60,40 +75,8 @@ class Frame:
         corners = cv.cornerSubPix(gray, np.float32(centroids), (5, 5), (-1, -1), criteria)
 
         for i in range(corners.shape[0]):
-            self.feature_points_vec.append(self.Point(corners[i, 0], corners[i, 1]))
-
-        # Now draw them
-        # res = np.hstack((centroids, corners))
-        # res = np.int0(res)
-
-        # img[res[:,1],res[:,0]]=[0,0,255]
-        # img[res[:, 3], res[:, 2]] = [0, 255, 0]
-        # plt.figure()
-        # plt.imshow(img)
-        # plt.show()
-
-        # return np.int0(corners)
-
-
-# def calcAffineMatrix(reference_points, shifted_points):
-#     reference_mat = np.zeros((6, 6))
-#     res_vec = np.zeros(6)
-#
-#     for i in range(3):
-#         reference_mat[2 * i, 0] = reference_points[i, 0]  # x_r
-#         reference_mat[2 * i, 1] = reference_points[i, 1]  # y_r
-#         reference_mat[2 * i, 4] = 1  # b1
-#
-#         reference_mat[2 * i + 1, 2] = reference_points[i, 0]  # x_r
-#         reference_mat[2 * i + 1, 3] = reference_points[i, 1]  # y_r
-#         reference_mat[2 * i + 1, 5] = 1  # b2
-#
-#         res_vec[2 * i] = shifted_points[i, 0]  # x
-#         res_vec[2 * i + 1] = shifted_points[i, 1]  # y
-#
-#     affine_parameters_vec = np.linalg.solve(reference_mat, res_vec)
-#
-#     return affine_parameters_vec
+            point = self.Point(corners[i, 0], corners[i, 1])
+            self.feature_points_vec.append(point)
 
 
 def initChosenFeatures(corners):
@@ -111,8 +94,8 @@ class SourceFrame(Frame):
     CoupledPoints = namedtuple('CoupledPoints', ['src_point', 'dst_point'])
 
     def __init__(self, src_img, dst_img_vec):
-        super().__init__(0, src_img)
-        self.frame_vec = [Frame(k, pic) for k, pic in enumerate(dst_img_vec)]
+        super().__init__(0, src_img, src_img)
+        self.frame_vec = [Frame(k, pic, src_img) for k, pic in enumerate(dst_img_vec)]
         self.frame_num = len(self.frame_vec)
         self.coupled_points = {k: [] for k in range(self.frame_num)}
         self.affine_mat = {k: np.empty((2, 3)) for k in range(self.frame_num)}
@@ -140,7 +123,9 @@ class SourceFrame(Frame):
     def SearchFeaturePoints(src_point, L, dst_point_vec):
         points_in_range = []
         for potential_point in dst_point_vec:
-            if np.all(abs(np.asarray([potential_point.x - src_point.x, potential_point.y - src_point.y])) <= L / 2):
+            x_dist = abs(potential_point.x - src_point.x)
+            y_dist = abs(potential_point.y - src_point.y)
+            if np.all(abs(np.asarray([x_dist, y_dist])) <= L / 2):
                 points_in_range.append(potential_point)
 
         return points_in_range
@@ -160,7 +145,8 @@ class SourceFrame(Frame):
 
     def AutomaticMatch(self, dst_frame_idx, L, W):
         dst_frame = self.frame_vec[dst_frame_idx]
-        for src_point in self.feature_points_vec:
+
+        for k, src_point in enumerate(self.feature_points_vec):
             try:
                 points_in_range = self.SearchFeaturePoints(src_point, L, dst_frame.feature_points_vec)
                 if len(points_in_range) == 0:
@@ -171,13 +157,17 @@ class SourceFrame(Frame):
                 else:
                     best_point = self.FindBestPoint(src_point, dst_frame, points_in_range, W)
 
-                self.AddCoupledPoints(dst_frame.idx, self.CoupledPoints(src_point, best_point))
+                self.AddCoupledPoints(dst_frame_idx, self.CoupledPoints(src_point, best_point), 255 - 10 * k, k)
             except SourceFrameError:
                 self.feature_points_vec.remove(src_point)
                 continue
 
-    def AddCoupledPoints(self, dst_frame_idx, coupled_points):
+    def AddCoupledPoints(self, dst_frame_idx, coupled_points, color, k):
         self.coupled_points[dst_frame_idx].append(coupled_points)
+
+        if k < 6:
+            self.ZeroPixelsInWindow(coupled_points.src_point, 10, self.frame_vec[dst_frame_idx].reference_img, color)
+            self.ZeroPixelsInWindow(coupled_points.dst_point, 10, self.frame_vec[dst_frame_idx].img, color)
 
     def RANSAC(self, dst_frame_idx):
         biggest_inlier = []
@@ -191,17 +181,19 @@ class SourceFrame(Frame):
             dst_points_vec = [couple.dst_point for couple in self.coupled_points[dst_frame_idx]]
             source_trans = [self.ApplyAffineTrans(source_point, M) for source_point in src_points_vec]
             points_dist = calcEuclideanDist(source_trans, dst_points_vec)
-            inlier_idx = np.where(points_dist < 10)[0]
+            inlier_idx = np.where(points_dist < 30)[0]
             inlier_group = [src_points_vec[idx] for idx in inlier_idx]
 
             if len(inlier_group) > len(biggest_inlier):
                 biggest_inlier = inlier_group
+                match_points = [source_trans[idx] for idx in inlier_idx]
                 best_M = M
                 best_iM = iM
                 best_dist = points_dist
 
         self.affine_mat[dst_frame_idx] = best_M
         self.affine_inv_mat[dst_frame_idx] = best_iM
+
 
     def applyAffineTrans(self):
         rows, cols, ch = self.img.shape
@@ -227,7 +219,6 @@ def calcEuclideanDist(estimated_points_list, real_points_list):
     dist_list = [np.linalg.norm(estimated_point - real_point, 2) for estimated_point, real_point in
                  zip(estimated_points_list, real_points_list)]
     return np.asarray(dist_list)
-
 
 # def reconstractImgs(chosen_features, list_of_frames):
 #     reference_pts = chosen_features[0, :, :]
@@ -263,17 +254,30 @@ class FrameError(ValueError):
     pass
 
 
-if __name__ == '__main__':
+def section3(source_frame):
+    frames = source_frame.frame_vec
+    for frame in frames:
+        plt.subplot(121), plt.imshow(frame.reference_img), plt.title('reference')
+        plt.subplot(122), plt.imshow(frame.img), plt.title('frame')
+        plt.show()
 
+
+if __name__ == '__main__':
+    extractImages('pen.mp4', 'extractedImgs')
+    # makeVideoMask('extractedImgs')
+    # createVideo('extractedImgs', 'masks', 'masked_pen.avi', 30)
+    # extractImages('masked_pen.avi', 'masked_extracted')
     frames_num_manual = list(range(20, 100, 15))
-    frames_num = list(range(20, 150, 20))
+    frames_num = list(range(85))
     frames_names = ['extractedImgs/frame' + "%03d" % num + '.jpg' for num in frames_num]
     frames = [cv.imread(im) for im in frames_names]
 
     source_frame = SourceFrame(frames[0], frames[1:])
     for i in range(source_frame.frame_num):
-        source_frame.AutomaticMatch(i, 50, 40)
+        source_frame.AutomaticMatch(i, 100, 40)
         source_frame.RANSAC(i)
+
+    # section3(source_frame)
 
     source_frame.applyAffineTrans()
     source_frame.applyInvAffineTrans()
@@ -282,7 +286,10 @@ if __name__ == '__main__':
     #     output = source_frame.affine_imgs[i]
     #     plotRconstImg(source_frame.img, output, source_frame.frame_vec[i].img)
     #
-    for i in range(source_frame.frame_num):
-        output = source_frame.inv_affine_imgs[i]
-        plotRconstImg(source_frame.frame_vec[i].img, output, source_frame.img)
+    # for i in range(source_frame.frame_num):
+    #     output = source_frame.inv_affine_imgs[i]
+    #     plotRconstImg(source_frame.frame_vec[i].img, output, source_frame.img)
+    #
+    stabilized_img = [source_frame.inv_affine_imgs[i] for i in range(source_frame.frame_num)]
+    createVideoFromList(stabilized_img,'stabi.avi', 20)
     print('all done')
--- Q3.py
+++ Q3.py
@@ -4,13 +4,13 @@ import numpy as np
 from sklearn.cluster import KMeans
 
 
-def extractImages(video_path):
+def extractImages(video_path, dst_folder):
     vidcap = cv.VideoCapture(video_path)
     success, image = vidcap.read()
     count = 0
     success = True
     while success:
-        cv.imwrite("extractedImgs/frame%03d.jpg" % count, image)     # save frame as JPEG file
+        cv.imwrite(dst_folder + "/frame%03d.jpg" % count, image)     # save frame as JPEG file
         success, image = vidcap.read()
         count += 1
 
@@ -25,6 +25,7 @@ def makeImMask(im):
     obj_label = np.argmin(label_pix)
     mask = np.zeros(clt.labels_.shape)
     mask[np.where(clt.labels_ == obj_label)] = 1
+    mask = cv.GaussianBlur(mask, (0, 0), 5)
     mask = np.uint8(mask.reshape((im.shape[0], im.shape[1])) * 255)
 
     return mask
@@ -53,6 +54,20 @@ def createVideo(image_folder, mask_folder, video_name, fps):
         proc_im = np.uint8(curr_im * curr_mask)
         cv.imwrite("vid/vid%03d.jpg" % i, proc_im)  # save frame as JPEG file
         video.write(proc_im)
+
+    cv.destroyAllWindows()
+    video.release()
+
+
+def createVideoFromList(image_list, video_name, fps):
+
+    height, width, layers = image_list[0].shape
+
+    fourcc = cv.VideoWriter_fourcc(*'XVID')
+    video = cv.VideoWriter(video_name, fourcc, fps, (width, height))
+
+    for image in image_list:
+        video.write(image)
 
     cv.destroyAllWindows()
     video.release()
